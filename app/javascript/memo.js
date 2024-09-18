c0fa7a3c610140e193f8248911c53245eb3e15a2
yukisan04 <yuki28yukinko2525@icloud.com>
2024-09-18 14:28:45 +0900

    コントローラーにリクエストを送信する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,17 @@
 function post (){
+<<<<<<< Updated upstream
   //リクエストを送信する処理
+=======
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+>>>>>>> Stashed changes
 };
 
 window.addEventListener('turbo:load', post);
